3c7d8d05b7a941a997e9a01011fd5c81708b1b43
TriggerStorm <michael.h.p@hotmail.com>
2020-09-09 14:13:02 +0200

    Initial commit

---
+++ PetShop.UI/Program.cs
@@ -0,0 +1,32 @@
+﻿using PetShop.Core.AppService;
+using PetShop.Core.AppService.Service;
+using PetShop.Core.DomainService;
+using PetShop.InfraStructure.Data;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PetShop.UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<IPetRepository, PetRepository>();
+            serviceCollection.AddScoped<IPetService, PetService>();
+            serviceCollection.AddScoped<IPrinter, Printer>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var printer = serviceProvider.GetRequiredService<IPrinter>();
+            FakeDB.InitData();
+            printer.StartUi();
+
+            //IPetRepository petRepository = new PetRepository();
+            //IPetService petService = new PetService(petRepository);
+            //var printer = new Printer(petService);
+
+            
+
+        }
+    }
+}
